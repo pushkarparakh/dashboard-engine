dc585e8259dfb460f6f6ca9a50f7aa2b7b7257e8
Pushkar Parakh <parakhpushkar1@gmail.com>
2026-04-29 09:39:29 +0530

    fix: restore utils functions overwritten by shadcn

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDate(date: string | Date) {
+  return new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  }).format(new Date(date));
+}
+
+export function generateId() {
+  return Math.random().toString(36).substr(2, 9);
+}
